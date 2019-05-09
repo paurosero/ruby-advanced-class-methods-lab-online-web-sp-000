abeb556263dfe110e27eb1cb13d09dd3411c1d4a
Paul Rosero <paul_rosero@yahoo.com>
2019-05-09 20:12:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,6 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def initlialize (name)
-    @name = name
-    self.class.all << self
-    
   def self.all
     @@all
   end
@@ -15,26 +11,60 @@ class Song
   end
   
   def self.create
-    song = self.new
-    @@all << song.save
+    song = Song.new
+    song.save
     song
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = name
+    song.name = song_name
     song
   end
   
-  def self.create_by_name(name)
-   song = self.new
-   @@all << song.save
-   song.name = name
+  def self.create_by_name(song_name)
+   song = self.create
+   song.name = song_name
    song
   end
   
-  def self.find_by_name(name)
-    self.all.find {|person| person.name == name}
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+  
+  def self.find_or_create_by_name(song_name)
+  self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+  
+  def self.alphabetical
+    self.all.sort_by {|song_name| song_name.name}
+  end
+  
+  def self.new_from_filename(filename)
+    seperate_name = filename.split(" - ")
+    artist_name = seperate_name[0]
+    song_name = seperate_name[1].chomp(".mp3")
+    
+    song = self.new 
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+  
+  def self.create_from_filename(filename)
+    seperate_name = filename.split(" - ")
+    artist_name = seperate_name[0]
+    song_name = seperate_name[1].chomp(".mp3")
+    
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end  
+  
+  def self.destroy_all
+    @@all.clear
+    ## self.all.clear
   end
 
 end
