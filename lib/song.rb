cfeb4eb69dc9191688e360b7334679ee5406c43a
Paul Rosero <paul_rosero@yahoo.com>
2019-05-09 19:58:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
     seperate_name = filename.split(" - ")
     song = self.new 
     artist_name = seperate_name[0]
-    song_name = seperate_name[1].chomp()
+    song_name = seperate_name[1].chomp(".mp3")
   end
 
 end
