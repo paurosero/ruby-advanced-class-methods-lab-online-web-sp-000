00f09bbe84c64c36a56483a8251c634028cf00fb
Paul Rosero <paul_rosero@yahoo.com>
2019-05-09 19:20:43 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,9 +16,9 @@ class Song
     song
   end
   
-  def self.new_by_name(name)
+  def self.new_by_name(song_name)
     song = self.new
-    song.name = name
+    song.name = song_name
     song
   end
   
